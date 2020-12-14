55542d36c1271fe8b708763a4fb025580241488f
Vane-Perez <70921200+Vane-Perez@users.noreply.github.com>
2020-12-13 20:39:54 -0600

    pagina1.php

---
+++ pagina1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UFT-8">
+	<meta name="Viewport" content="width=device-width, initial-scale=1.0">
+	<title>Parcial-Software Libre</title>
+</head>
+<body>
+	<form methond="post" action="pagina2.php">
+Por favor, envia la newsletter a: <br>
+Tu Nombre:
+<input type="text" name="Nombre"><br>
+Tu Apellido:
+<input type="text" name="Apellido"><br>
+Tu Correo Electronico:
+<input type="text" name="email"><br>
+<input type="submit" value="Enviar Formulario">
+</form>
+</body>
+</html>
